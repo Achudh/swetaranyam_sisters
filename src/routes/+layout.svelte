c19591de6c196d6194a52a0bc0105a927db1bf11
Achudh Krishna A G <achudhk@gmail.com>
2025-10-05 01:46:20 +0530

    Add @vercel/analytics dependency and update package-lock.json; refactor layout to inject analytics based on environment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	import Layout from '$lib/components/layout/Layout.svelte';
+	import Layout from "$lib/components/layout/Layout.svelte";
+	import { dev } from "$app/environment";
+	import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+	injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <Layout>
