3ff6f6969fac1be669f521cce55f9003b2efe980
Achudh Krishna A G <achudhk@gmail.com>
2025-10-06 11:26:33 +0530

    Add Veil component to layout for conditional rendering based on URL parameters; enhance user experience by allowing content reveal.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
 	import Layout from "$lib/components/layout/Layout.svelte";
 	import { dev } from "$app/environment";
+	import Veil from "$lib/components/ui/Veil.svelte";
 	import { injectAnalytics } from "@vercel/analytics/sveltekit";
-
+	let showVeil = false;
+	if (typeof window !== "undefined") {
+		const params = new URLSearchParams(window.location.search);
+		showVeil = params.get("reveal") === "true";
+	}
 	injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <Layout>
+	{#if showVeil}
+		<Veil on:end={() => (showVeil = false)} />
+	{:else}
 	<slot />
+	{/if}
 </Layout>
+<!-- <script>
+	import Veil from '$lib/components/ui/Veil.svelte';
+	let showVeil = false;
+  
+	if (typeof window !== 'undefined') {
+	  const params = new URLSearchParams(window.location.search);
+	  showVeil = params.get('reveal') === 'true';
+	}
+  </script>
+  
+  {#if showVeil}
+	<Veil on:end={() => (showVeil = false)} />
+  {/if}
+  
+  <slot />
+   -->
